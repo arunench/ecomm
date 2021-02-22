ae2b578f757254e49a3d2e52c6ca27a9e1685358
Arunen Chellan <arunenchellan@Arunens-MacBook-Pro.local>
2021-02-21 20:47:06 -0500

    First commit

---
+++ routes/admin/auth.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+
+const usersRepo = require('../../repositories/users');
+const signupTemplate = require('../../views/admin/auth/signup');
+const signinTemplate = require('../../views/admin/auth/signin');
+const { requireEmail, 
+        requirePassword, 
+        requirePasswordConfirmation,
+        requireEmailExists,
+        requireValidPasswordForUser } = require('./validators');
+const users = require('../../repositories/users');
+
+const router = express.Router();
+
+router.get('/signup', (req, res) => {
+
+    res.send(signupTemplate({ req }));
+
+});
+
+
+router.post(
+    '/signup', 
+    [
+        requireEmail,
+        requirePassword,
+        requirePasswordConfirmation
+    ],
+async (req, res) => {
+    const errors = validationResult(req);
+    
+    if (!errors.isEmpty()){
+        return res.send(signupTemplate({ req, errors }));
+    }
+
+    const { email, password, passwordConfirmation } = req.body;
+    const user = await usersRepo.create({ email, password });
+    
+    // We created this property, cookie session manages req.session and adds it to the res object
+    req.session.userId = user.id;
+
+    res.send('Account created!!!');
+});
+
+router.get('/signout', (req, res) => {
+    req.session = null;
+    res.send('You are logged out');
+
+});
+
+router.get('/signin', (req, res) => {
+    res.send(signinTemplate({}));
+});
+
+router.post('/signin', [requireEmailExists,requireValidPasswordForUser], 
+    async (req, res) => {
+        const errors = validationResult(req);
+        
+        if(!errors.isEmpty()){
+            return res.send(signinTemplate({ errors }));
+        }
+
+        const { email, password } = req.body;
+
+        const user = await usersRepo.getOneBy({ email });
+
+        if (!user){
+            return res.send('Email not found');
+        }
+
+
+        req.session.userId = user.id;
+
+        res.send('You are signed in!!!');
+});
+
+module.exports = router;
