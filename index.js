0b177c78adf67967f0ece93ebbe5994a58dc09b6
Arunen Chellan <arunench@gmail.com>
2021-03-05 14:34:20 -0500

    added port

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.use(
 app.use(authRouter);
 app.use(productsRouter);
 
-app.listen(3000, () =>{
+const port = process.env.port || 3000;
+app.listen(port, () =>{
     console.log("listening");
 });
 
