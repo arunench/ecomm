40913749a0f46f38a21a2fd7eb6129f0089a067c
Arunen Chellan <arunench@gmail.com>
2021-03-05 13:50:01 -0500

    Added logic for products routes

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
 
+const { handleErrors } = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -27,20 +27,16 @@ router.post(
         requirePassword,
         requirePasswordConfirmation
     ],
-async (req, res) => {
-    const errors = validationResult(req);
-    
-    if (!errors.isEmpty()){
-        return res.send(signupTemplate({ req, errors }));
-    }
+    handleErrors(signupTemplate),
+    async (req, res) => {
 
-    const { email, password, passwordConfirmation } = req.body;
-    const user = await usersRepo.create({ email, password });
-    
-    // We created this property, cookie session manages req.session and adds it to the res object
-    req.session.userId = user.id;
+        const { email, password } = req.body;
+        const user = await usersRepo.create({ email, password });
+        
+        // We created this property, cookie session manages req.session and adds it to the res object
+        req.session.userId = user.id;
 
-    res.send('Account created!!!');
+        res.redirect('/admin/products');
 });
 
 router.get('/signout', (req, res) => {
@@ -53,26 +49,18 @@ router.get('/signin', (req, res) => {
     res.send(signinTemplate({}));
 });
 
-router.post('/signin', [requireEmailExists,requireValidPasswordForUser], 
+router.post('/signin', 
+    [requireEmailExists,requireValidPasswordForUser], 
+    handleErrors(signinTemplate),
     async (req, res) => {
-        const errors = validationResult(req);
-        
-        if(!errors.isEmpty()){
-            return res.send(signinTemplate({ errors }));
-        }
 
-        const { email, password } = req.body;
-
+        const { email } = req.body;
+        console.log(req.body);
         const user = await usersRepo.getOneBy({ email });
-
-        if (!user){
-            return res.send('Email not found');
-        }
-
 
         req.session.userId = user.id;
 
-        res.send('You are signed in!!!');
+        res.redirect('/admin/products');
 });
 
 module.exports = router;
